de7cb66e8ac7620fed31faa66158bdcf62f04c4b
vaishali M <va17300212@gmail.com>
2025-05-08 22:24:18 +0530

    Update twostring.java

--- java/twostring.java
+++ java/twostring.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class twoloop {
+    public static void main(String[] args) {
+        Scanner vaishu =new Scanner(System.in);
+        System.out.print("enter any number:");
+        int a=vaishu.nextInt();
+        int i,j;
+        for(i=499,j=1 ; i>=j ; i-- , j++){
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
 
